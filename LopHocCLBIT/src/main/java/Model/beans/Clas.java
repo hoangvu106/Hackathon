0a717fdcf6750f9c283e25924dbc890855ed5551
THANHDAT\Administrator <tdatlqd@gmail.com>
2016-12-25 11:03:39 +0700

    no message

--- LopHocCLBIT/src/main/java/Model/beans/Clas.java
+++ LopHocCLBIT/src/main/java/Model/beans/Clas.java
@@ -11,7 +11,43 @@ package Model.beans;
  * @author DatNguyen
  */
 public class Clas {
-    private String id;
+    private String id_class;
+
+    public Clas() {
+    }
     private String name;
-    
+    private String id_teacher;
+    private String location;
+
+    public String getId_class() {
+        return id_class;
+    }
+
+    public void setId_class(String id_class) {
+        this.id_class = id_class;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId_teacher() {
+        return id_teacher;
+    }
+
+    public void setId_teacher(String id_teacher) {
+        this.id_teacher = id_teacher;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
